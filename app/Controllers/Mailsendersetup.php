29540c995cdb0d8b8fe2c3894fce1c9ed6cdb7d8
TrianggaR <triangga.restu@gmail.com>
2023-04-13 13:15:10 +0700

    mailsender setup
    mailsender setup

---
+++ app/Controllers/Mailsendersetup.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\Login_model;
+use App\Models\Administration_model;
+use App\Models\Notif_model;
+use App\Models\Setup_model;
+//use App\Models\Settingnavheader_model;
+
+
+class Mailsendersetup extends BaseController
+{
+
+    private $nav_data;
+    private $header_data;
+    private $footer_data;
+    private $db_name;
+    public function __construct()
+    {
+        //parent::__construct();
+        helper('form', 'url');
+        $this->db_name = \Config\Database::connect();
+
+        $this->LoginModel = new Login_model();
+        $this->AdministrationModel = new Administration_model();
+        $this->NotifModel = new Notif_model();
+        $this->SetupModel = new Setup_model();
+        //$this->SettingnavheaderModel = new Settingnavheader_model();
+        if (empty(session()->get('keylog'))) {
+            //Tidak Bisa menggunakan redirect kalau condition session di construct
+            //return redirect()->to(base_url('/login'));
+            header('Location: ' . base_url());
+            exit();
+        } else {
+            $user = session()->get('username');
+            $chksu = $this->LoginModel->datalevel($user);
+            if ($chksu == 0) {
+                //header("HTTP/1.1 401 Unauthorized"); //Status Code 401 = Unauthorized
+                header('Location: ' . base_url('administration'));
+                exit();
+            } else {
+
+                $infouser = $this->LoginModel->datapengguna($user);
+                $mailbox_unread = $this->NotifModel->get_mailbox_unread($user);
+                $this->header_data = [
+                    'usernamelgn'   => $infouser['usernamelgn'],
+                    'namalgn' => $infouser['namalgn'],
+                    'emaillgn' => $infouser['emaillgn'],
+                    'issuperuserlgn' => $infouser['issuperuserlgn'],
+                    'notif_messages' => $mailbox_unread,
+                ];
+                $this->footer_data = [
+                    'usernamelgn'   => $infouser['usernamelgn'],
+                ];
+                // Assign the model result to the badly named Class Property
+                $activenavd = 'mailsendersetup';
+                $activenavh = 'mailsetup';
+                $this->nav_data = [
+                    'active_navd' => $activenavd,
+                    'active_navh' => $activenavh,
+                    'menu_nav' => $this->AdministrationModel->get_navigation(),
+                    //'ttl_inbox_unread' => $this->AdministrationModel->count_message(),
+                    //'chkusernav' => $this->AdministrationModel->count_navigation($user), 
+                    //'active_navh' => $this->AdministrationModel->get_activenavh($activenavd),
+                ];
+                //}
+            }
+        }
+    }
+
+
+    public function index()
+    {
+        $mailsender_data = $this->SetupModel->get_mailsender();
+        $nav['act_tab'] = 1;
+        $data = array(
+            'mailsender_data' => $mailsender_data,
+            'form_action' => base_url("mailsendersetup/update_mailsender"),
+        );
+
+        echo view('view_header', $this->header_data);
+        echo view('view_nav', $this->nav_data);
+        echo view('settings/tab_menu', $nav);
+        echo view('settings/mailsender_form', $data);
+        echo view('view_footer', $this->footer_data);
+    }
+
+    public function notifsetup()
+    {
+        $mailsender_data = $this->SetupModel->get_mailsender();
+        $nav['act_tab'] = 2;
+        $data = array(
+            'mailsender_data' => $mailsender_data,
+            'form_action' => base_url("mailsendersetup/update_mailsender"),
+        );
+
+        echo view('view_header', $this->header_data);
+        echo view('view_nav', $this->nav_data);
+        echo view('settings/tab_menu', $nav);
+        echo view('settings/mailsender_notifsetup', $data);
+        echo view('view_footer', $this->footer_data);
+    }
+
+    public function update_mailsender()
+    {
+        if (!$this->validate([
+            'sender_hostname' => 'required|valid_url',
+            'sender_name' => 'required|alpha_space',
+            'sender_email' => 'required|valid_email',
+            'sender_password' => 'required',
+            'sender_ssl' => 'required|alpha',
+            'sender_smtpport' => 'required|numeric',
+
+        ])) {
+            $id = $this->request->getPost('id');
+            session()->setFlashdata('messagefailed', 'Input Failed, Complete data before save!..');
+            return redirect()->to(base_url('/mailsendersetup'))->withInput();
+        } else {
+            $today = date("d/m/Y");
+            $audtdate = substr($today, 6, 4) . "" . substr($today, 3, 2) . "" . substr($today, 0, 2);
+            $id = $this->request->getPost('id');
+            $data = array(
+                'AUDTDATE' => $audtdate,
+                'AUDTTIME' => $audtdate,
+                //'AUDTUSER'=> $audtdate,
+                'AUDTORG' => $this->db_name->database,
+                'HOSTNAME' => $this->request->getPost('sender_hostname'),
+                'SENDERNAME' => $this->request->getPost('sender_name'),
+                'SENDEREMAIL' => $this->request->getPost('sender_email'),
+                'PASSWORDEMAIL' => $this->request->getPost('sender_password'),
+                'SSL' => $this->request->getPost('sender_ssl'),
+                'SMTPPORT' => $this->request->getPost('sender_smtpport'),
+            );
+
+
+            $this->SetupModel->mailsender_update($id, $data);
+            session()->setFlashdata('messagesuccess', 'Update Data Success');
+            return redirect()->to(base_url('/mailsendersetup'));
+            //print_r($data);
+        }
+    }
+}
---
+++ app/Views/settings/mailsender_form.php
@@ -0,0 +1,107 @@
+										<div class="tab-pane <?php if ($act_tab == 1) : ?>active<?php endif ?>">
+											<div class="row">
+												<div class="col-md-12">
+													<div class="box-header with-border">
+														<h3 class="box-title"><strong>E-mail Sender </strong></h3>
+													</div>
+													<div class="box-body">
+														<div class="row">
+															<div class="col-md-8">
+
+																<form id="validasi" action="<?= $form_action ?>" method="POST" class="form-horizontal">
+																	<table class="table table-striped">
+																		<tr>
+																			<td class="col-sm-6 text-right">Server Hostname : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm "><i class="fa fa-globe"></i></span>
+																					<input id="sender_hostname" name="sender_hostname" class="form-control input-sm required" type="text" placeholder="Navigation Name" value="<?php echo set_value($mailsender_data['HOSTNAME']); ?>" maxlength="50"></input>
+																				</div>
+																			</td>
+																		</tr>
+																		<tr>
+																			<td class="col-sm-6 text-right">E-mail Name : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm "><i class="fa fa-user"></i></span>
+																					<input id="sender_name" name="sender_name" class="form-control input-sm required" type="text" placeholder="Navigation Name" value="<?= ($mailsender_data['SENDERNAME'])
+																																																						?>" maxlength="50"></input>
+																				</div>
+																			</td>
+																		</tr>
+																		<tr>
+																			<td class="col-sm-6 text-right">E-mail : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm ">@</span>
+																					<input id="sender_email" name="sender_email" class="form-control input-sm required" type="text" placeholder="Navigation Name" value="<?= ($mailsender_data['SENDEREMAIL'])
+																																																							?>" maxlength="50"></input>
+																			</td>
+																		</tr>
+																		<tr>
+																			<td class="col-sm-6 text-right">Password : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm "><i class="fa fa-lock"></i></span>
+																					<input id="sender_password" name="sender_password" class="form-control input-sm required" type="text" placeholder="Navigation Name" value="<?= ($mailsender_data['PASSWORDEMAIL']) ?>" maxlength="50"></input>
+																					<span class="input-group-addon input-sm "><i class="fa fa-eye"></i></span>
+																				</div>
+
+																			</td>
+																		</tr>
+																		<tr>
+																			<td class="col-sm-6 text-right">Encryption Method : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm "><i class="fa fa-expeditedssl"></i></span>
+																					<input id="sender_ssl" name="sender_ssl" class="form-control input-sm required" type="text" placeholder="Navigation Name" value="<?= ($mailsender_data['SSL']) ?>" maxlength="10" readonly></input>
+																				</div>
+																			</td>
+																		</tr>
+
+																		<tr>
+																			<td class="col-sm-6 text-right">SMTP Port : </td>
+																			<td class="col-sm-6">
+																				<div class="input-group">
+																					<span class="input-group-addon input-sm "><i class="fa fa-expeditedssl"></i></span>
+																					<input id="sender_smtpport" name="sender_smtpport" class="form-control input-sm required" type="text" placeholder="SMTP Port" value="<?= ($mailsender_data['SMTPPORT']) ?>" maxlength="4"></input>
+																				</div>
+																			</td>
+																		</tr>
+																		<tr>
+
+																			<td colspan="2" class="col-sm-12 text-center"><code>This E-mail will be the master e-mail to sending all notification from this apps.</code>
+																			</td>
+																		</tr>
+																		<tr>
+
+																			<td colspan="2" class="col-sm-12 text-center">
+																				<p class="text-muted text-red well well-sm no-shadow" style="margin-top: 10px;">
+																					<small><strong><i class="fa fa-info-circle text-red"></i><?= validation_list_errors() ?></strong></small>
+																				</p>
+																			</td>
+																		</tr>
+																		<tr>
+																			<td colspan="2" class="col-sm-12 text-right">
+																				<input type="hidden" id="id" name="id" value="<?= $mailsender_data['ID'] ?>">
+																				<button type='submit' class='btn btn-social btn-flat btn-info btn-sm confirm'><i class='fa fa-check'></i> Update Settings</button>
+																			</td>
+																		</tr>
+																	</table>
+																</form>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</div>
+										</section>
+										</div>
