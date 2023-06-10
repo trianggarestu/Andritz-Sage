048911d8626f805b6c8bf6b3ea992dbb46eeb800
TrianggaR <triangga.restu@gmail.com>
2023-06-10 23:22:59 +0700

    Update Construct OT
    Update Construct OT

--- app/Controllers/OrderTracking.php
+++ app/Controllers/OrderTracking.php
@@ -37,7 +37,7 @@ class OrderTracking extends BaseController
         } else {
             $user = session()->get('username');
             $infouser = $this->LoginModel->datapengguna($user);
-            if (session()->get('keylog') == $infouser['passlgn']) {
+            if (session()->get('keylog') == $infouser['passlgn'] and session()->get('userhash') == $infouser['userhashlgn']) {
 
                 $mailbox_unread = $this->NotifModel->get_mailbox_unread($user);
                 $this->header_data = [
@@ -45,6 +45,8 @@ class OrderTracking extends BaseController
                     'namalgn' => $infouser['namalgn'],
                     'emaillgn' => $infouser['emaillgn'],
                     'issuperuserlgn' => $infouser['issuperuserlgn'],
+                    'photolgn' => $infouser['photolgn'],
+                    'userhashlgn' => $infouser['userhashlgn'],
                     'notif_messages' => $mailbox_unread,
                 ];
                 $this->footer_data = [
