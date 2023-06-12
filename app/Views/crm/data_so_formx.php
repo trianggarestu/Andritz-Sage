0cce1eefe5fc39466af9579f5050d489018024d9
TrianggaR <triangga.restu@gmail.com>
2023-06-12 09:31:54 +0700

    Rev Test - Error PO
    Rev Test - Error PO

--- app/Views/crm/data_so_formx.php
+++ app/Views/crm/data_so_formx.php
@@ -1,3 +1,6 @@
+<script type="text/javascript" src="<?= base_url() ?>assets/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="<?= base_url() ?>assets/js/validasi.js"></script>
+<script type="text/javascript" src="<?= base_url() ?>assets/js/localization/messages_id.js"></script>
 <style>
 	.input-sm {
 		padding: 4px 4px;
@@ -19,18 +22,28 @@
 			<li class="active">Form</li>
 		</ol>
 	</section>
+
+	<input id="success-code" type="hidden" value="<?= $success_code ?>">
+	<!-- Untuk menampilkan modal bootstrap umum  -->
+	<div class="modal fade" id="modalBox" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class='modal-dialog'>
+			<div class='modal-content'>
+
+				<div class="fetched-data"></div>
+			</div>
+		</div>
+	</div>
 	<section class="content" id="maincontent">
 		<form action="<?= $form_action;
-						?>" method="post">
+						?>" method="post" id="validasi">
 			<div class="row">
 				<div class="col-md-12">
 					<div class="box box-info">
 						<div class="box-header with-border">
-							<a href="<?= base_url(); ?>salesorder" title="Reset Data" onclick="<?php echo base_url(); ?>" class="btn btn-social btn-flat bg-purple btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"><i class='fa fa-refresh'></i> Reset Sales Order Form</a>
-
-							<a href="#" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Back to Sales Order List">
-								<i class="fa fa-arrow-circle-left "></i>Back to Sales Order List
+							<a href="<?= base_url(); ?>salesorderopen" class="btn btn-social btn-flat btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block" title="Back to Sales Order Open">
+								<i class="fa fa-arrow-circle-left "></i>Back to Sales Order Open
 							</a>
+							<a href="<?= base_url(); ?>salesorder" title="Reset Data" onclick="<?php echo base_url(); ?>" class="btn btn-social btn-flat bg-olive btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"><i class='fa fa-refresh'></i> Reset Sales Order Form</a>
 						</div>
 
 						<div class="box-body">
@@ -48,57 +61,70 @@
 							<div class="row">
 								<div class="col-sm-12">
 									<div class="form-group subtitle_head">
-										<label class="text-right"><strong>Select Contract :</strong></label>
+										<label class="text-right"><strong>CONTRACT :</strong></label>
 									</div>
 								</div>
+
 								<div class='col-sm-4'>
 									<div class='form-group'>
-										<label>Contract </label>
-										<div class="input-group <?= ($validation->hasError('ct_no')) ? 'has-error' : ''; ?>">
+										<label for="ct_no">Contract </label>
+										<div class="input-group">
 											<span class="input-group-addon input-sm"><a href="<?= base_url(); ?>salesorder/form_select_contractopen/" data-remote="false" data-toggle="modal" data-target="#modalBox"><i class="fa fa-search"></i></a></span>
 											<input type="text" maxlength="10" size="10" class="form-control input-sm required" name="ct_no" id="ct_no" placeholder="Contract Number" value="<?= $ct_no ?>" readonly>
-											<span class="help-block"><?= $validation->getError('ct_no'); ?></span>
+
 
 										</div></input>
 									</div>
 								</div>
 								<div class='col-sm-6'>
-									<label>Contract Description </label>
-									<input type="text" class="form-control input-sm required" id="ct_desc" name="ct_desc" placeholder="" value="<?= $ct_desc; ?>" readonly />
+									<div class='form-group'>
+										<label for="ct_desc">Contract Description </label>
+										<input type="text" class="form-control input-sm required" id="ct_desc" name="ct_desc" placeholder="" value="<?= $ct_desc; ?>" readonly />
+									</div>
 								</div>
 
 								<div class='col-sm-2'>
 									<div class="form-group">
-										<label>Sales Person</label>
-										<input type="text" class="form-control input-sm required" id="ct_salesperson" name="ct_salesperson" placeholder="" value="<?= $ct_salesperson; ?>" readonly />
+										<label for="ct_salesperson">Sales Person</label>
+										<input type="text" class="form-control input-sm required" id="ct_salesperson" name="ct_salesperson" placeholder="" value="<?= $ct_salesperson; ?>" <?php if ($ct_salesperson == '') {
+																																																echo '';
+																																															} else {
+																																																echo 'readonly';
+																																															} ?> />
 									</div>
 								</div>
 
 							</div>
 							<div class="row">
 								<div class='col-sm-2'>
-									<label>Customer No. </label>
-									<input type="text" class="form-control input-sm required" id="ct_custno" name="ct_custno" placeholder="" value="<?= $ct_custno; ?>" readonly />
+									<div class="form-group">
+										<label for="ct_custno">Customer No. </label>
+										<input type="text" class="form-control input-sm required" id="ct_custno" name="ct_custno" placeholder="" value="<?= $ct_custno; ?>" readonly />
+									</div>
 								</div>
 								<div class='col-sm-2'>
-									<label>Customer Email </label>
-									<input type="text" class="form-control input-sm required" id="ct_email" name="ct_email" placeholder="" value="<?= $ct_email; ?>" readonly />
+									<div class="form-group">
+										<label for="ct_email">Customer Email </label>
+										<input type="text" class="form-control input-sm required" id="ct_email" name="ct_email" placeholder="" value="<?= $ct_email; ?>" readonly />
+									</div>
 								</div>
 								<div class='col-sm-8'>
-									<label>Customer Name </label>
-									<input type="text" class="form-control input-sm required" id="ct_namecust" name="ct_namecust" placeholder="" value="<?= $ct_custname; ?>" readonly />
+									<div class="form-group">
+										<label for="ct_namecust">Customer Name </label>
+										<input type="text" class="form-control input-sm required" id="ct_namecust" name="ct_namecust" placeholder="" value="<?= $ct_custname; ?>" readonly />
+									</div>
 								</div>
 
 							</div>
 							<div class="row">
 								<div class="col-sm-12">
 									<div class="form-group subtitle_head">
-										<label class="text-right"><strong>Project :</strong></label>
+										<label class="text-right"><strong>PROJECT :</strong></label>
 									</div>
 								</div>
 								<div class='col-sm-4'>
 									<div class='form-group'>
-										<label>Project </label>
+										<label for="prj_no">Project </label>
 										<div class="input-group">
 											<span class="input-group-addon input-sm"><a href="<?= base_url() . 'salesorder/form_select_project_by_contract/' . $ct_no; ?>/" data-remote="false" data-toggle="modal" data-target="#modalBox"><i class=" fa fa-search"></i></a></span>
 											<input type="text" maxlength="10" size="10" class="form-control input-sm required" id="prj_no" name="prj_no" placeholder="Project Number" value="<?= $prj_no; ?>" readonly>
@@ -106,80 +132,138 @@
 									</div>
 								</div>
 								<div class='col-sm-4'>
-									<label>Description </label>
-									<input type="text" class="form-control input-sm required" id="prj_desc" name="prj_desc" placeholder="" value="<?= $prj_desc; ?>" readonly />
-								</div>
-								<div class='col-sm-2'>
-									<label>PO Customer <code> (auto filled) </code> </label>
-									<input type="text" class="form-control input-sm required" id="po_cust" name="po_cust" placeholder="" value="<?= $po_cust; ?>" <?php if ($po_cust == '') {
-																																										echo '';
-																																									} else {
-																																										echo 'readonly';
-																																									} ?> />
-								</div>
-								<div class='col-sm-2'>
-									<label>PO Date <code> (auto filled) </code> </label>
-									<div class="input-group input-group-sm date">
-										<div class="input-group-addon">
-											<i class="fa fa-calendar"></i>
-										</div>
-										<input type="text" class="form-control input-sm required" id="prj_startdate" name="prj_startdate" placeholder="" value="<?= $prj_startdate; ?>" readonly />
-									</div>
-
-								</div>
-
-							</div>
-							<div class="row">
-								<div class='col-sm-2'>
-									<label>CRM Number<code> (manual input)) </code> </label>
-									<input type="text" class="form-control input-sm required" id="crm_no" name="crm_no" placeholder="input here.." value="" />
-								</div>
-
-								<div class='col-sm-2'>
-
 									<div class='form-group'>
-										<label>Req. Date <code> (manual input) </code> </label>
+										<label for="prj_desc">Description </label>
+										<input type="text" class="form-control input-sm required" id="prj_desc" name="prj_desc" placeholder="" value="<?= $prj_desc; ?>" readonly />
+									</div>
+								</div>
+								<div class='col-sm-2'>
+									<div class='form-group'>
+										<label for="po_cust">PO Customer <code> (auto filled) </code> </label>
+										<input type="text" class="form-control input-sm required" id="po_cust" name="po_cust" placeholder="" value="<?= $po_cust; ?>" <?php if ($po_cust == '') {
+																																											echo '';
+																																										} else {
+																																											echo 'readonly';
+																																										} ?> />
+									</div>
+								</div>
+								<div class='col-sm-2'>
+									<div class='form-group'>
+										<label for="prj_startdate">PO Date <code> (auto filled) </code> </label>
 										<div class="input-group input-group-sm date">
 											<div class="input-group-addon">
 												<i class="fa fa-calendar"></i>
 											</div>
-											<input class="datepicker form-control input-sm pull-right" id="req_date" name="req_date" type="text" value="">
+											<input type="text" class="form-control input-sm required" id="prj_startdate" name="prj_startdate" placeholder="" value="<?= $prj_startdate; ?>" readonly />
 										</div>
 									</div>
-								</div>
-								<div class='col-sm-4'>
-									<label>Order Description <code> (manual input) </code> </label>
-									<input type="text" class="form-control input-sm required" id="ord_desc" name="ord_desc" placeholder="input here.." value="" />
-								</div>
-								<div class='col-sm-2'>
-									<label>Service Type <code> (Auto Filled) </code> </label>
-									<input type="text" class="form-control input-sm required" id="so_service" name="so_service" value="<?= $prj_startdate; ?>" readonly />
-								</div>
-								<div class='col-sm-2'>
-									<label>Remarks <code> (manual input) </code> </label>
-									<input type="text" class="form-control input-sm required" id="so_remarks" name="so_remarks" placeholder="input here.." value="" />
 								</div>
 
 							</div>
 							<div class="row">
-								<div class='col-sm-4'>
+								<div class="col-sm-12">
+									<div class="form-group subtitle_head">
+										<label class="text-right"><strong>CRM :</strong></label>
+									</div>
+								</div>
+								<div class='col-sm-2'>
 									<div class='form-group'>
-										<label>Inventory No </label>
-										<input type="text" class="form-control input-sm required" id="inventory_no" name="inventory_no" placeholder="input here.." value="">
+										<label for="crm_no">CRM Number<code> (manual input) </code> </label>
+										<input type="text" class="form-control input-sm required" id="crm_no" name="crm_no" placeholder="input here.." value="<?= $crm_no ?>" />
+
+									</div>
+								</div>
+								<div class='col-sm-2'>
+
+									<div class='form-group'>
+										<label for="req_date">Req. Date <code> (manual input) </code> </label>
+										<div class="input-group input-group-sm date">
+											<div class="input-group-addon">
+												<i class="fa fa-calendar"></i>
+											</div>
+											<input class="datepicker form-control input-sm required" id="req_date" name="req_date" type="text" value="<?= $req_date ?>" readonly>
+										</div>
 									</div>
 								</div>
 								<div class='col-sm-4'>
-									<label>Material No <code> (auto filled) </code> </label>
-									<input type="text" class="form-control input-sm required" id="material_no" name="material_no" placeholder="" value="" />
+									<div class='form-group'>
+										<label for="ord_desc">Order Description <code> (manual input) </code> </label>
+										<input type="text" class="form-control input-sm required" id="ord_desc" name="ord_desc" placeholder="input here.." value="<?= $order_desc ?>" />
+									</div>
+								</div>
+								<div class='col-sm-4'>
+									<div class='form-group'>
+										<label for="so_remarks">Remarks <code> (manual input) </code> </label>
+										<input type="text" class="form-control input-sm required" id="so_remarks" name="so_remarks" placeholder="input here.." value="<?= $order_remarks ?>" />
+									</div>
 								</div>
 
+							</div>
+							<div class="row">
 								<div class='col-sm-2'>
-									<label>Qty <code> (manual input) </code> </label>
-									<input type="text" class="form-control input-sm required|numeric" id="so_qty" name="so_qty" placeholder="input here.." value="" />
+									<div class='form-group'>
+										<label for="so_service">Service Type <code> (choose) </code> </label>
+										<select name="so_service" class="form-control input-sm required">
+											<option value="">--Choose One--</option>
+											<option value="SPAREPARTS" <?php if ($service_type == "SPAREPARTS") {
+																			echo "selected";
+																		} ?>>SPAREPARTS</option>
+											<option value="SERVICES" <?php if ($service_type == "SERVICES") {
+																			echo "selected";
+																		} ?>>SERVICES</option>
+
+										</select>
+									</div>
+								</div>
+								<div class='col-sm-4'>
+									<div class='form-group'>
+										<label for="inventory_no">Inventory No</label>
+										<select class="form-control input-sm select2 required" id="inventory_no" name="inventory_no" style="width:100%;">
+											<option option value="">--SELECT INVENTORY NO--</option>
+											<?php foreach ($item_data as $icitem) :
+											?>
+												<option value="<?= trim($icitem['ITEMNO'])
+																?>" <?php if ($inventory_no == trim($icitem['ITEMNO'])) {
+																		echo "selected";
+																	} ?>><?= trim($icitem['ITEMNO'])
+																			?> - <?= $icitem['ITEMDESC']
+																					?>
+												</option>
+											<?php endforeach;
+											?>
+										</select>
+									</div>
 								</div>
 								<div class='col-sm-2'>
-									<label>Uom <code> (manual input) </code> </label>
-									<input type="text" class="form-control input-sm required" id="so_uom" name="so_uom" placeholder="input here.." value="" />
+									<div class='form-group'>
+										<label for="material_no">Material No <code> (auto filled) </code> </label>
+										<input type="text" class="form-control input-sm required" id="material_no" name="material_no" placeholder="" value="<?= $material_no ?>" />
+									</div>
+								</div>
+								<div class='col-sm-2'>
+									<div class='form-group'>
+										<label for="so_qty">Qty <code> (manual input) </code> </label>
+										<input type="text" class="form-control input-sm required" id="so_qty" name="so_qty" placeholder="input here.." value="<?= $qty ?>" />
+									</div>
+								</div>
+								<div class='col-sm-2'>
+									<div class='form-group'>
+										<label for="so_uom">Uom <code> (Choose) </code> </label>
+										<select name="so_uom" class="form-control input-sm required">
+											<option value="">--Choose One--</option>
+											<option value="Ea" <?php if ($stock_unit == "Ea") {
+																	echo "selected";
+																} ?>>Ea</option>
+											<option value="Pcs" <?php if ($stock_unit == "Pcs") {
+																	echo "selected";
+																} ?>>Pcs</option>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class='col-sm-12'>
+									<?= validation_list_errors() ?>
 								</div>
 							</div>
 						</div>
@@ -192,8 +276,10 @@
 				</div>
 				<div class='box-footer'>
 					<div class='col-xs-12'>
+						<input type="hidden" id="ct_manager" name="ct_manager" value="<?= $ct_manager ?>">
+						<input type="hidden" id="csr_uniq" name="csr_uniq" value="<?= $csr_uniq ?>">
 						<button type='reset' class='btn btn-social btn-flat btn-danger btn-sm'><i class='fa fa-times'></i> Cancel</button>
-						<button type='submit' class='btn btn-social btn-flat btn-info btn-sm pull-right'><i class='fa fa-check'></i> Saved & Sent Notif</button>
+						<button type='submit' class='btn btn-social btn-flat btn-info btn-sm pull-right'><i class='fa fa-check'></i> Save</button>
 					</div>
 				</div>
 
